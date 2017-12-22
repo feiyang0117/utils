4359495b5de7aa40302282d18fad70f518724486
gaomiaofei <gaomiaofei@shanqb.com>
2017-12-22 22:52:40 +0800

    deep clone

--- deepClone.js
+++ deepClone.js
@@ -1,3 +1,26 @@
 /**
  * Created by gao on 22/12/2017.
  */
+/** es5深度克隆写法 **/
+exports.deepClone =  function (obj, newObj) {
+    var newObj = newObj || {};
+    for(var i in obj){
+        if(typeof obj[i] == 'object'){
+            newObj[i] = obj[i].constructor == Arrary ? [] : {}
+            this.deepClone(obj[i], newObj[i]);
+        } else {
+            newObj[i] = obj[i]
+        }
+    }
+    return newObj;
+};
+
+/** JSON深度克隆 **/
+exports.JsonDeepClone= function (obj) {
+    let o = JSON.parse(JSON.stringify(obj));
+    if(typeof obj == 'object' || typeof obj == 'arrary' || typeof obj == 'string' || typeof obj == 'number'){
+        return o;
+    } else {
+        return `type: ${typeof obj}`;
+    }
+};
