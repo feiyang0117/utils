f352e1db80029b2b92e0fdab0d2386d33ba01b02
gaomiaofei <gaomiaofei@shanqb.com>
2017-12-22 23:46:12 +0800

    sort 随机排序

--- common.js
+++ common.js
@@ -2,7 +2,7 @@
  * Created by gao on 22/12/2017.
  */
 /** es5深度克隆写法 **/
-exports.deepClone =  function (obj, newObj) {
+exports.deepClone = (obj, newObj) => {
     var newObj = newObj || {};
     for(var i in obj){
         if(typeof obj[i] == 'object'){
@@ -16,11 +16,21 @@ exports.deepClone =  function (obj, newObj) {
 };
 
 /** JSON深度克隆 **/
-exports.JsonDeepClone= function (obj) {
+exports.JsonDeepClone= (obj) => {
     if(typeof obj == 'object' || typeof obj == 'arrary' || typeof obj == 'string' || typeof obj == 'number'){
         let o = JSON.parse(JSON.stringify(obj));
         return o;
     } else {
         return `type: ${typeof obj}`;
     }
+};
+
+/** 关于数组随机排序 **/
+exports.arrRandom = (arr, callback) => {
+    if(arr.constructor !== Array) return;
+    this.fn = (...a) => {
+        return Math.random() > 0.5 ? -1 : 1;
+    };
+    let newArr  = arr.sort(this.fn);
+    callback(newArr);
 };
